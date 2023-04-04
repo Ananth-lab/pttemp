3d0f65affe37ba8ca400470cea29c18f24cea2c8
Ananth <anantharajabk@gmail.com>
2023-04-04 17:51:08 +0530

    minor fix

--- parentauth/src/users/tenant_organisation_address/tenant_organisation_address.service.ts
+++ parentauth/src/users/tenant_organisation_address/tenant_organisation_address.service.ts
@@ -25,27 +25,27 @@ constructor(@InjectRepository(TenantOrganisationAddress) private readonly repoOr
    const orgAdd=await this.repoOrAd.findOne({where:{id}})
    if(!orgAdd) return orgAdd
    if (updateTenantOrganisationAddressDto.name) {
-    updateTenantOrganisationAddressDto.name = updateTenantOrganisationAddressDto.name;
+    orgAdd.name = updateTenantOrganisationAddressDto.name;
   }
   if (updateTenantOrganisationAddressDto.address) {
-    updateTenantOrganisationAddressDto.address = updateTenantOrganisationAddressDto.address;
+    orgAdd.address = updateTenantOrganisationAddressDto.address;
   }
   if (updateTenantOrganisationAddressDto.plot_no) {
-   updateTenantOrganisationAddressDto.plot_no = updateTenantOrganisationAddressDto.plot_no;
+    orgAdd.plot_no = updateTenantOrganisationAddressDto.plot_no;
   }
   if (updateTenantOrganisationAddressDto.city) {
-   updateTenantOrganisationAddressDto.city = updateTenantOrganisationAddressDto.city;
+    orgAdd.city = updateTenantOrganisationAddressDto.city;
   }
   if (updateTenantOrganisationAddressDto.state) {
-   updateTenantOrganisationAddressDto.state = updateTenantOrganisationAddressDto.state;
+    orgAdd.state = updateTenantOrganisationAddressDto.state;
   }
   if (updateTenantOrganisationAddressDto.post_code) {
-   updateTenantOrganisationAddressDto.post_code = updateTenantOrganisationAddressDto.post_code;
+    orgAdd.post_code = updateTenantOrganisationAddressDto.post_code;
   }
   if (updateTenantOrganisationAddressDto.country) {
-   updateTenantOrganisationAddressDto.country = updateTenantOrganisationAddressDto.country;
+    orgAdd.country = updateTenantOrganisationAddressDto.country;
   }
-  return await this.repoOrAd.save(updateTenantOrganisationAddressDto);
+  return await this.repoOrAd.save(orgAdd);
 } catch (error) {
   throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
 }
