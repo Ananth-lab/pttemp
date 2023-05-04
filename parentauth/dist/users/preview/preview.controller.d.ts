e617779ce7d0f41dd17ba6cce75d36a572558138
Ananth <anantharajabk@gmail.com>
2023-05-04 14:28:06 +0530

    minor change

--- parentauth/dist/users/preview/preview.controller.d.ts
+++ parentauth/dist/users/preview/preview.controller.d.ts
@@ -1,11 +1,15 @@
 import { TenantOrganisationAddressService } from "../tenant_organisation_address/tenant_organisation_address.service";
 import { TenantPocService } from "../tenant_poc/tenant_poc.service";
 import { SubscriptionService } from "src/subscription/subscription.service";
+import { TmodulesService } from "src/tmodules/tmodules.service";
+import { TsubmodulesService } from "src/tmodules/tsubmodules.service";
 export declare class PreviewController {
     private readonly tenantAddress;
     private readonly tenantPoc;
     private readonly tenantSubscription;
-    constructor(tenantAddress: TenantOrganisationAddressService, tenantPoc: TenantPocService, tenantSubscription: SubscriptionService);
+    private readonly tenantModule;
+    private readonly tenantSubModule;
+    constructor(tenantAddress: TenantOrganisationAddressService, tenantPoc: TenantPocService, tenantSubscription: SubscriptionService, tenantModule: TmodulesService, tenantSubModule: TsubmodulesService);
     findOne(id: string): Promise<import("../tenant_organisation_address/entities/tenant_organisation_address.entity").TenantOrganisationAddress>;
     finalSubmit(data: any): Promise<any>;
 }
